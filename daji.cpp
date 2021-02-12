98ce19010cd9c628b3c6c11e561ccf2f281a51b8
kingofvictory <64626864+kingofvictory@users.noreply.github.com>
2021-02-12 16:32:46 +0800

    add source file

---
+++ daji.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::string start = "\e[1;33;4;41m";
+    std::string end   = "\e[m";
+
+    std::cout << start << "牛年大吉！"       << end << "\n";
+    std::cout << start << "  牛年吉祥！"     << end << "\n";
+    std::cout << start << "    心想事成！"   << end << "\n";
+    std::cout << start << "      牛气冲天！" << end << "\n";
+    std::cout << start << "       牛运势！"  << end << "\n";
+    std::cout << start << "     牛运气！"    << end << "\n";
+    std::cout << start << "    牛气象！"     << end << "\n";
+    std::cout << start << "  牛势力！"       << end << "\n";
+    std::cout << start << "牛！"             << end << "\n";
+
+    return 0;
+}
